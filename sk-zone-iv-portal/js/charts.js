daa7b0efe1e4a263f36b8405f97324763f7ccb94
danajll <santiagodanajill@gmail.com>
2026-07-01 19:39:07 +0800

    POLISHED FINAL - D

---
+++ sk-zone-iv-portal/js/charts.js
@@ -0,0 +1,85 @@
+const chartCanvas = document.getElementById("budgetChart");
+
+if (chartCanvas) {
+
+    new Chart(chartCanvas, {
+
+        type: "doughnut",
+
+        data: {
+
+            labels: [
+
+                "Education",
+
+                "Sports",
+
+                "Health",
+
+                "Governance"
+
+            ],
+
+            datasets: [{
+
+                data: [
+
+                    90000,
+
+                    88700,
+
+                    10000,
+
+                    250000
+
+                ],
+
+                backgroundColor: [
+
+                    "#0057B8",
+
+                    "#2F80ED",
+
+                    "#7EC8FF",
+
+                    "#DCEEFF"
+
+                ],
+
+                borderWidth: 0,
+
+                hoverOffset: 12
+
+            }]
+
+        },
+
+        options: {
+
+            responsive: true,
+
+            maintainAspectRatio: false,
+
+            cutout: "70%",
+
+            plugins: {
+
+                legend: {
+
+                    display: false
+
+                },
+
+                tooltip: {
+
+                    enabled: true
+
+                }
+
+            }
+
+        }
+
+    });
+
+}
